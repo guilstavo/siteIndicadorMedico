29f491795c3751fe8ccc94b077a188fa40c0452e
Gustavo <gustavomartinis@gmail.com>
2016-01-19 12:33:51 -0200

    Adicionado arquivo style.css e logo.

--- application/views/header.php
+++ application/views/header.php
@@ -4,10 +4,15 @@
 	<meta charset="UTF-8">
 	<title>Document</title>
 	<link rel="stylesheet" href="<?= base_url("css/bootstrap.css") ?>">
+	<link rel="stylesheet" href="<?= base_url("css/style.css") ?>">
 </head>
 <body>
 	<div class="container">
-		
+		<?= anchor('http://apmsantos.org.br/indicadorMedico/', img(array(
+          'src' => 'images/logo.jpg',
+          'alt' => 'APM Santos',
+          'title' => 'APM Santos'
+		)), 'class="logo"' ) ?>
 		<?php if($this->session->flashdata("success")) : ?>
 			<p class="alert alert-success"><?= $this->session->flashdata("success") ?></p>
 		<?php endif ?>
