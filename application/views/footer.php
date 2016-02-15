960d0f0079f51b67710f70d959dfbd1c1b992815
Gustavo <gustavomartinis@gmail.com>
2016-02-15 16:41:51 -0200

    Criado footer e alterada cores do site com as cores de identidade da APM.

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,3 +1,26 @@
 </div>
+<footer>
+	<div class="container">
+		<div id="footer_left">
+			<h4>Associação Paulista de Medicina - Santos</h4>
+				<address>
+		        <p>Av. Ana Costa, 388 - CEP 11060-002 - Santos/SP</p>
+		        <p>Atendimento ao Associado:</p>
+		        <p>Segunda à Sexta : 9h às 20h / Sábado: 9h à 13h</p>
+		        <p>Telefone: (13) 3289-2626</p>
+		        <p>Email: santos@apm.org.br</a>
+		        <p>&copy; 2016. APM Santos.</p>
+		    </address>
+	    </div>
+	    <div id="redes_footer">
+	    	<h4>APM Santos nas Redes Sociais</h4>
+	        <ul id="lista_redes_sociais">
+	            <li><a title="Curta a APM Santos no Facebook" href="http://www.facebook.com/apmsantos.org.br" target="_blank"> <img width="40px" alt="Facebook" src="/images/facebook.png"> </a></li>
+	            <li><a title="Siga a APM Santos no Twitter" href="https://twitter.com/#!/apmsantos_org" target="_blank"> <img width="40px" alt="Twitter" src="/images/twitter.png"> </a></li>
+	            <li><a title="Acompanhe o Canal da APM Santos no Youtube" href="http://www.youtube.com/user/TVAPMSantos/videos?flow=grid&amp;view=1" target="_blank"><img width="40px" alt="You Tube" src="/images/youtube.png"> </a></li>
+	        </ul>
+	    </div>
+	</div>
+</footer>
 </body>
 </html>
